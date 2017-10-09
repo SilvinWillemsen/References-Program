0ec591f5e1f972e1ca757bd5246555caf91508cd
SilvinWillemsen <silvinwillemsen@gmail.com>
2017-10-09 15:44:50 +0200

    Initial Commit

---
+++ References Program/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  References Program
+//
+//  Created by Silvin Willemsen on 09/10/2017.
+//  Copyright © 2017 Silvin Willemsen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
